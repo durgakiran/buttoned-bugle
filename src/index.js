e9661de47e2d6ef7927b5ab2f151f6493db5d40f
Glitch (create-react-app-sample) <none>
2019-08-08 01:31:51 +0000

    🗾🚩 Checkpoint ./package.json:5807719/2749 ./src/index.js:5807719/219

--- src/index.js
+++ src/index.js
@@ -7,3 +7,12 @@ ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+//ReactDOM.render(what I want to render, where I want to render)
+
+//JSX is javascript version of html
+//JSX should always have a wrapper element
+
+
+var myNewP = document.createElement("p")
+myNewP.innerH
