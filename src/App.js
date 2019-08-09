65e648d03ac94bcb67dbe04e07530eccf6702211
Glitch (buttoned-bugle) <none>
2019-08-09 01:44:15 +0000

    🚵💎 Checkpoint ./src/todo.css:5807719/326 ./src/App.js:5807719/883 ./src/TodoItem.js:5807719/28

--- src/App.js
+++ src/App.js
@@ -2,20 +2,37 @@ import React, { Component } from 'react';
 import './App.css';
 import toDoData from "./toDoData"
 import TodoItem from "./TodoItem"
+import './todo.css';
 
 
 /**
  * we can only add className to actual dom elements
  * we should not add className to custom elements
  */
-function App() {
+// function App() {
   
-  const toDoItems = toDoData.map( data => <TodoItem  key= {data.id} item={data}/>)
+//   const toDoItems = toDoData.map( data => <TodoItem  key= {data.id} item={data}/>)
+//     return (
+//       <div className="todo-list">
+//         {toDoItems}
+//       </div>
+//     );
+// }
+
+class App extends React.Component {
+  
+  generateItems() {
+    const toDoItems = toDoData.map( data => <TodoItem  key= {data.id} item={data}/>)
+    return toDoItems;
+  }
+  render() {
+    const toDoItems = this.generateItems();
     return (
       <div className="todo-list">
         {toDoItems}
       </div>
-    );
+    )
+  }
 }
 
 export default App;
